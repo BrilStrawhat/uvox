bc2e7d42f04a40b92d20900409340124fa7d1f63
BrilStrawhat <37308329+BrilStrawhat@users.noreply.github.com>
2020-12-27 21:59:19 +0200

    Update pwm_notes.c

--- components/accelerometer/pwm_notes.c
+++ components/accelerometer/pwm_notes.c
@@ -4,60 +4,59 @@
 #include "driver/ledc.h"
 #include "esp_err.h"
 
-#define LEDC_CHANNEL_3         3
-#define LEDC_LS_CH3_GPIO       (25)
+#define LEDC_CHANNEL_1          3
+#define LEDC_LS_CH1_GPIO        25
 
-#define LEDC_HS_TIMER          LEDC_TIMER_0
-#define LEDC_HS_MODE           LEDC_HIGH_SPEED_MODE
-#define LEDC_LS_CH3_CHANNEL    LEDC_CHANNEL_3
-#define LEDC_LS_TIMER          LEDC_TIMER_1
-#define LEDC_LS_MODE           LEDC_LOW_SPEED_MODE
+#define LEDC_LS_CH1_CHANNEL     LEDC_CHANNEL_1
+#define LEDC_LS_TIMER           LEDC_TIMER_1
+#define LEDC_LS_MODE            LEDC_LOW_SPEED_MODE
 
-#define LEDC_TEST_CH_NUM       (4)
-#define LEDC_TEST_DUTY         (20)
-#define LEDC_TEST_FADE_TIME    (3000)
+#define DEFAULT_DUTY            0x0
+#define MIN_DUTY                5
 
-void notes(void)
-{
-    /*
-     * Prepare and set configuration of timers
-     * that will be used by LED Controller
-     */
+#define B_NOTE                  246
+#define Cis_1OCTAL_NOTE         277
+#define D_1OCTAL_NOTE           293
+#define Fis_1OCTAL_NOTE         369
+#define A_1OCTAL_NOTE           440
+#define B_1OCTAL_NOTE           493
+
+
+void pwm_notes(void) {
     ledc_timer_config_t ledc_timer = {
         .duty_resolution = LEDC_TIMER_13_BIT, // resolution of PWM duty
-        .freq_hz = 262,                      // frequency of PWM signal
+        .freq_hz = B_NOTE,                      // frequency of PWM signal
         .speed_mode = LEDC_LS_MODE,           // timer mode
         .timer_num = LEDC_LS_TIMER,            // timer index
         .clk_cfg = LEDC_AUTO_CLK,              // Auto select the source clock
     };
-    // Set configuration of timer0 for high speed channels
-    ledc_timer_config(&ledc_timer);
     ledc_channel_config_t ledc_channel = {
-        .channel    = LEDC_LS_CH3_CHANNEL,
+        .channel    = LEDC_LS_CH1_CHANNEL,
         .duty       = 0,
-        .gpio_num   = LEDC_LS_CH3_GPIO,
+        .gpio_num   = LEDC_LS_CH1_GPIO,
         .speed_mode = LEDC_LS_MODE,
         .hpoint     = 0,
         .timer_sel  = LEDC_LS_TIMER
     };
     ledc_channel_config(&ledc_channel);
+    ledc_timer_config(&ledc_timer);
 
-    // Initialize fade service.
-    ledc_fade_func_install(0);
-
-    ledc_set_duty(ledc_channel.speed_mode, ledc_channel.channel, LEDC_TEST_DUTY);
-    ledc_update_duty(ledc_channel.speed_mode, ledc_channel.channel);
-    vTaskDelay(1000 /portTICK_PERIOD_MS);
-    ledc_set_freq(LEDC_LS_MODE, LEDC_LS_TIMER, 293);
-    vTaskDelay(1000 /portTICK_PERIOD_MS);
-    ledc_set_freq(LEDC_LS_MODE, LEDC_LS_TIMER, 293);
-    vTaskDelay(1000 /portTICK_PERIOD_MS);
-    ledc_set_freq(LEDC_LS_MODE, LEDC_LS_TIMER, 329);
-    vTaskDelay(1000 /portTICK_PERIOD_MS);
-    ledc_set_freq(LEDC_LS_MODE, LEDC_LS_TIMER, 349);
-    vTaskDelay(1000 /portTICK_PERIOD_MS);
-    ledc_set_freq(LEDC_LS_MODE, LEDC_LS_TIMER, 391);
-    vTaskDelay(1000 /portTICK_PERIOD_MS);
-    ledc_set_freq(LEDC_LS_MODE, LEDC_LS_TIMER, 440);
-    // }
+    int delay = 50;
+    int i = 0xff;
+    for (i = 0; i < 0xffff; i += 16) {
+        printf("%x\n", DEFAULT_DUTY + i);
+        ledc_set_duty(ledc_channel.speed_mode, ledc_channel.channel, DEFAULT_DUTY + i);
+        ledc_update_duty(ledc_channel.speed_mode, ledc_channel.channel);
+        ledc_set_freq(LEDC_LS_MODE, LEDC_LS_TIMER, 494); 
+        vTaskDelay(delay /portTICK_PERIOD_MS);
+        // // ledc_set_freq(LEDC_LS_MODE, LEDC_LS_TIMER, Cis_1OCTAL_NOTE);
+        // // vTaskDelay(delay /portTICK_PERIOD_MS);
+        // ledc_set_freq(LEDC_LS_MODE, LEDC_LS_TIMER, D_1OCTAL_NOTE);
+        // vTaskDelay(delay /portTICK_PERIOD_MS);
+        // ledc_set_freq(LEDC_LS_MODE, LEDC_LS_TIMER, Fis_1OCTAL_NOTE);
+        // vTaskDelay(delay /portTICK_PERIOD_MS);
+        // // ledc_set_freq(LEDC_LS_MODE, LEDC_LS_TIMER, A_1OCTAL_NOTE);
+        // // vTaskDelay(delay /portTICK_PERIOD_MS);
+        // // ledc_set_freq(LEDC_LS_MODE, LEDC_LS_TIMER, B_1OCTAL_NOTE);
+    }
 }
